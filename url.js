5a77168658bc4c68a5c70d8f14a6eaef636155dc
vivian0123 <a0973025215@gmail.com>
2025-07-09 09:11:49 +0800

    Update url.js

--- url.js
+++ url.js
@@ -8,7 +8,7 @@
 
 
 const urls = [
-	'https://hhming.moe',
-	'https://github.com/hms5232',
-	'https://gitlab.com/hms5232',
+	'https://www.surveycake.com/s/ModRl',
+	'https://www.surveycake.com/s/KX68v',
+	
 ];
